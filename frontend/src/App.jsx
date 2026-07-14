0debc1e5a04bfbb4de1cf4d65c169aa0721326f9
Aditya Kumar Dwivedi <adityadwivedi525@gmail.com>
2026-07-14 15:14:42 +0530

    impoved videoMeet UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,12 +29,14 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/landing';
 import Authentication from './pages/authentication';
+import VideoMeetComponent from './pages/VideoMeet.jsx';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/auth" element={<Authentication />} />
+      <Route path='/:url' element={<VideoMeetComponent />} />
     </Routes>
   );
 }
